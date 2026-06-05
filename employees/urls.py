e7a6ff7d34e9297070422108d3a5da884972788d
dattatritekale <dattatritekale2@gmail.com>
2026-06-05 16:20:36 +0530

    Added update employee API

--- employees/urls.py
+++ employees/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import employee_list, create_employee
+from .views import employee_list, create_employee, update_employee, delete_employee
 
 urlpatterns=[
-    path('employee/',employee_list),
+    path('employee/<int:id>/',employee_list),
     path('employee/create/',create_employee),
+    path('employee/<int:id>/update/', update_employee),
+    path('employee/<int:id>/delete/', delete_employee),
 ]
--- employees/views.py
+++ employees/views.py
@@ -6,12 +6,22 @@ from .serializers import EmployeeSerializer
 # Create your views here.
 
 @api_view(['GET'])
-def employee_list(request):
-    employees=Employee.objects.all()
+def employee_list(request,id):
 
-    serializer=EmployeeSerializer(employees,many=True)
+    try:
+        employees=Employee.objects.get(id=id)
     
-    return Response(serializer.data)
+
+        serializer=EmployeeSerializer(employees)
+    
+        return Response(serializer.data)
+    
+    except Employee.DoesNotExist:
+
+        return Response(
+            {"error":"Emploee not found"},
+            status=404
+        )
 
 @api_view(['POST'])
 def create_employee(request):
@@ -23,3 +33,43 @@ def create_employee(request):
         return Response(serializer.data)
     
     return Response(serializer.errors)
+
+@api_view(['PUT'])
+def update_employee(request, id):
+    try:
+        employees=Employee.objects.get(id=id)
+
+    except Employee.DoesNotExist:
+        return Response(
+            {"messege":"Employee not found"},
+            status=404
+        )
+    
+    serializer=EmployeeSerializer(employees, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    
+    return Response(
+        serializer.errors,
+        status=400
+    )
+
+@api_view(['DELETE'])
+def delete_employee(reqest, id):
+    try:
+        employees=Employee.objects.get(id=id)
+
+    except Employee.DoesNotExist:
+        return Response(
+            {"error":"Employee does not exist"},
+            status=404
+        )
+    employees.delete()
+
+    return Response(
+        {"message":"Employee deleted successfully"},
+        status=200
+    )
+    
