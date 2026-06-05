9b750530b4ba950d720dc7ea9a924468c0ad4ac3
dattatritekale <dattatritekale2@gmail.com>
2026-06-05 12:38:26 +0530

    Added employee CRUD APIs

--- employees/urls.py
+++ employees/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import employee_list
+from .views import employee_list, create_employee
 
 urlpatterns=[
     path('employee/',employee_list),
+    path('employee/create/',create_employee),
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,20 +1,25 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Employee
+from .serializers import EmployeeSerializer
 
 # Create your views here.
 
 @api_view(['GET'])
 def employee_list(request):
-    employess=[
-         {
-            "id": 1,
-            "name": "Dattatri",
-            "role": "Python Developer"
-        },
-        {
-            "id": 2,
-            "name": "Rahul",
-            "role": "Backend Developer"
-        }
-    ]
-    return Response(employess)
+    employees=Employee.objects.all()
+
+    serializer=EmployeeSerializer(employees,many=True)
+    
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_employee(request):
+    
+    serializer= EmployeeSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    
+    return Response(serializer.errors)
