8c6719082e7fd0424a3860493e1e171a08e33743
dattatritekale <dattatritekale2@gmail.com>
2026-06-17 20:14:10 +0530

    Added Search Filter and Ordering Filter

--- employees/views.py
+++ employees/views.py
@@ -2,10 +2,26 @@ from rest_framework.response import Response
 from .models import Employee
 from .serializers import EmployeeSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
 
 
 # Create your views here.
 class EmployeeViewset(ModelViewSet):
     queryset=Employee.objects.all()
     serializer_class= EmployeeSerializer
+
+    filter_backends =[
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+
+    search_fields =[
+        'name',
+        'designation'
+    ]
+
+    ordering_fields=[
+        'name',
+        'created_at'
+    ]
 
